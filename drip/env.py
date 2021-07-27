6473afd5129abc93c07a6817791921a990204ff6
박준영 <sjwm98@gmail.com>
2021-07-27 15:52:26 +0900

    Add: jenkins file 추가

--- drip/env.py
+++ drip/env.py
@@ -2,8 +2,8 @@ import os
 
 from .production import *
 
-SECRET_KEY = os.environ['SECRET_KEY']
-ALGORITHM = os.environ['ALGORITHM']
-SECRET_ACCESS_KEY = os.environ['SECRET_ACCESS_KEY']
-DATABASES = os.envior['DATABASE']
+SECRET_KEY = os.environ.get('SECRET_KEY')
+ALGORITHM = os.environ.get('ALGORITHM')
+SECRET_ACCESS_KEY = os.environ.get('SECRET_ACCESS_KEY')
+DATABASES = os.envior.get('DATABASE')
 DEBUG = True
