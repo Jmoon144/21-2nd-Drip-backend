1661bc45c292d2e93b4a40d6417db815e7868c55
박준영 <sjwm98@gmail.com>
2021-07-27 15:26:53 +0900

    Add: jenkins file 추가

--- drip/production.py
+++ drip/production.py
@@ -1,20 +1,11 @@
 from .base import *
-
+from drip_settings import DATABASES, SECRET_KEY, ACCESS_KEY, SECRET_ACCESS_KEY, ALGORITHM
 DEBUG = False
 
-SECRET_KEY = 'django-insecure-2zdh6vdhr%eo8fx9*u=nl(_3yxa4c0#0nyh&ye%n-$wc9534#e'
-DATABASES = {
-    'default' : {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'drip2',
-        'USER': 'root',
-        'PASSWORD': 'go9511455',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
+SECRET_KEY = SECRET_KEY
+DATABASES = DATABASES
 
-ALGORITHM = "HS256"
+ALGORITHM = ALGORITHM
 
-ACCESS_KEY = "AKIAZBG6YDXQE4I2BEU6"
-SECRET_ACCESS_KEY = "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"
+ACCESS_KEY = ACCESS_KEY
+SECRET_ACCESS_KEY = SECRET_ACCESS_KEY
